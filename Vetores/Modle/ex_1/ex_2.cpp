ecbc8536edebfdaba488e82c070b31c4536ab513
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-11-06 17:19:02 -0300

    exercio 1 modle atualizado

--- Vetores/Modle/ex_1/ex_2.cpp
+++ Vetores/Modle/ex_1/ex_2.cpp
@@ -6,7 +6,6 @@ int main() {
 
     // Leitura dos valores do vetor
     for (int i = 0; i < tamanho; i++) {
-        std::cout << "Digite o elemento " << i + 1 << ": ";
         std::cin >> vetor[i];
     }
 
@@ -18,7 +17,6 @@ int main() {
     }
 
     // Impressão do vetor modificado
-    std::cout << "Vetor modificado:" << std::endl;
     for (int i = 0; i < tamanho; i++) {
         std::cout << vetor[i] << std::endl;
     }
--- Vetores/Modle/ex_1/ex_3.cpp
+++ Vetores/Modle/ex_1/ex_3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 int main() {
     const int tamanho = 12;
@@ -8,7 +9,6 @@ int main() {
 
     // Leitura dos valores do vetor
     for (int i = 0; i < tamanho; i++) {
-        std::cout << i + 1 ;
         std::cin >> vetor[i];
 
         if (vetor[i] > 0) {
@@ -17,12 +17,12 @@ int main() {
         }
     }
 
-    // Cálculo e impressão da média dos valores positivos
+    // Cálculo e impressão da média dos valores positivos com 2 casas decimais
     if (count > 0) {
         double media = soma / count;
-        std::cout << "Media dos elementos positivos: " << media << std::endl;
+        std::cout << "Media dos elementos positivos: " << std::fixed << std::setprecision(1) << media << std::endl;
     } else {
-        std::cout << "Não há elementos positivos no vetor." << std::endl;
+        std::cout << "Media dos elementos positivos: " << std::fixed << std::setprecision(1) << "0.00" << std::endl;
     }
 
     return 0;
