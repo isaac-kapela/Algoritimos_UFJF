44753c61a4e4ef752bf764aac52f1e1d99231be1
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-09-10 15:44:25 -0300

    add ex 01 struct

---
+++ 2°LC/Struct/Exercicios/ex_01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    struct academicos{
+        char nome[50];
+        char disciplina[40];
+        float nota_prova1;
+        float nota_prova2;
+    };
+                        academicos aluno;
+
+            for(int i =0; i < 5; i++){
+                cin.getline(aluno.nome, 50);
+                cin.getline(aluno.disciplina, 40);
+                cin >> aluno.nota_prova1;
+                cin >> aluno.nota_prova2;
+                cin.ignore();
+            
+        }
+
+          cout << "Nome ...........: " << aluno.nome << endl;
+                cout << "Disciplina .....: " << aluno.disciplina << endl;
+                cout << "Nota da Prova 1 ...: "  << fixed << setprecision(2) << aluno.nota_prova1 << endl;
+                cout << "Nota da Prova 2 ...:"  << fixed << setprecision(2) << aluno.nota_prova2 << endl;
+    
+    return 0;
+}
