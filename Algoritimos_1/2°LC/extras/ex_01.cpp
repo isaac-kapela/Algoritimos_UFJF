0cbb0b5b17f3c4cb81cc957883ab124771919837
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-11-06 18:53:41 -0300

    aula 1 ponteiros

--- Algoritimos_1/2°LC/extras/ex_01.cpp
+++ Algoritimos_1/2°LC/extras/ex_01.cpp
@@ -1,33 +1,61 @@
-    #include <iostream>
+#include <iostream>
 #include <iomanip>
 #include <cmath>
 
 using namespace std;
-#define  tam 100
-     int relacao(int matriz[][tam], int vet[], int total){
+#define TAM 100
+int relacao(int matriz[TAM][TAM], int vet[], int total)
+{
 
-            for(int i = 0; i < total; i++){
-                for(int j = 0; j < total; j++){
-                    if(i == j){
-                        matriz[i][j] = 0;
+    for (int i = 0; i < total; i++)
+    {
+        int soma = 0;
 
-                    }
-                    else if(matriz[1][2] && matriz[2][1]){
-                        matriz[i][j] = 1;
-                    }
-                    else{
-                        matriz[i][j] = 0;
-                    }
+        for (int j = 0; j < total; j++)
+        {
+            // if (i == j)
+            // {
 
-                }
-            }
+            // }
+            // else if (matriz[1][2] && matriz[2][1])
+            // {
+            //     matriz[i][j] = 1;
+            // }
+            // else
+            // {
+            //     matriz[i][j] = 0;
+            // }
+            soma = soma + matriz[i][j];
         }
+        vet[i] = soma;
+    }
+    int menor = 0; // vet[0];
 
+    for (int i = 0; i < total; i++)
+    {
+        if (vet[menor] < vet[i])
+        {
+            menor = i;
+        }
+    }
+    return menor;
+}
 
-    
+int main()
+{
+    int total;
+    cin >> total;
+    int vet[total];
+    int m[TAM][TAM];
 
+    for (int i = 0; i < total; i++)
+    {
+        for (int j = 0; j < total; j++)
+        {
+            cin >> m[i][j];
+        }
+    }
 
-
-int main(){
+    cout << relacao(m, vet, total);
     return 0;
 }
